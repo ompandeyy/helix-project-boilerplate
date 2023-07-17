c5c6f3eecc56e677976468d3286821b4b044977c
ompandeyy <ompandeyinfy@gmail.com>
2023-07-17 22:08:48 +0200

    Update partner-ecosystem.js

--- blocks/partner-ecosystem/partner-ecosystem.js
+++ blocks/partner-ecosystem/partner-ecosystem.js
@@ -18,7 +18,8 @@ newElement.innerHTML = `
           </div>
           <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <div class="row d-flex"></div>
+              <div class="row d-flex">
+              </div>
             </div>
           </div>
         </div>
@@ -27,51 +28,33 @@ newElement.innerHTML = `
   </section>
 `;
 
+// Get the container element for the logos
 const logosContainer = newElement.querySelector('.row.d-flex');
 
+// Loop through the logos and create the logo elements
 
 export default function decorate(block) {
-// Get the container element for the logos
-
-let colElement = document.createElement('div');
-colElement.className = 'col-lg-2 col-md-2 col-sm-4 col-xs-12';
-
- [...block.children].forEach((logosRow, index) => {
-  if (index > 0 && index % 5 === 0) {
-    logosContainer.appendChild(logosRow);
-    logosRow = document.createElement('div');
-    logosRow.className = 'row d-flex';
-  }
-
-  const logoUrl = logosRow.nextElementSibling.textContent;
-  const imageUrl = logosRow.querySelector('img').getAttribute('src');
-  const imageAlt = logosRow.querySelector('img').getAttribute('alt');
+[...block.children].forEach((logo, index) => {
+  const logoUrl = logo.nextElementSibling.textContent;
+  const imageUrl = logo.querySelector('img').getAttribute('src');
+  const imageAlt = logo.querySelector('img').getAttribute('alt');
 
   const logoElement = document.createElement('div');
-  logoElement.className = 'logo-design';
+  logoElement.className = 'col-lg-2 col-md-2 col-sm-4 col-xs-12';
+
   logoElement.innerHTML = `
-    <div class="logo-img-wrap">
-      <a href="${logoUrl}" title="${imageAlt}">
-        <img src="${imageUrl}" class="img-responsive get-image-height center-block" alt="${imageAlt}">
-      </a>
+    <div class="logo-design">
+      <div class="logo-img-wrap">
+        <a href="${logoUrl}" title="${imageAlt}">
+          <img src="${imageUrl}" class="img-responsive get-image-height center-block" alt="${imageAlt}">
+        </a>
+      </div>
     </div>
   `;
 
-  colElement.appendChild(logoElement);
-
-  if ((index + 1) % 5 === 0 || index === block.length - 1) {
-    logosRow.appendChild(colElement);
-    colElement = document.createElement('div');
-    colElement.className = 'col-lg-2 col-md-2 col-sm-4 col-xs-12';
-  }
-
-});
-// Add the last row of logos to the container
-logosContainer.appendChild(logosRow);
+  logosContainer.appendChild(logoElement);
+  });
 }
-
-
 
 // Replace the target element with the new element
 targetElement.parentNode.replaceChild(newElement, targetElement);
-
