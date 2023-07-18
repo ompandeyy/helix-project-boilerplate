31f2c61dfea6984daa3c6275097cbf37db311ced
ompandeyy <ompandeyinfy@gmail.com>
2023-07-18 11:38:49 +0200

    Update dx-carousel.js

--- blocks/dx-carousel/dx-carousel.js
+++ blocks/dx-carousel/dx-carousel.js
@@ -1,72 +1,77 @@
-// Get the target element to replace
-const targetElement = document.querySelector('.dx-carousel-wrapper');
+  // Get the target element
+  var targetElement = document.querySelector('.dx-carousel');
 
-// Create a new <div> element
-const newElement = document.createElement('div');
-newElement.className = 'carousel aem-GridColumn aem-GridColumn--default--12';
+  // Create the main container element for the converted carousel [2]
+  var mainContainer = document.createElement('div');
+  mainContainer.classList.add('carousel', 'aem-GridColumn', 'aem-GridColumn--default--12');
 
-// Set the HTML content for the new element
-newElement.innerHTML = `
-  <div class="cmp-carousel" data-cmp-delay="5000">
-    <section id="hero_slider">
-      <div id="main-cnt"></div>
-      <article class="container-fluid slider">
-        <div class="row">
-          <div id="hero_slider_carousel" class="owl-carousel owl-theme owl-loaded owl-drag">
-            <div class="owl-stage-outer">
-              <div class="owl-stage">
-                <div class="owl-item cloned" style="width: 1263.33px;"></div>
-              </div>
-            </div>
-            <div class="owl-nav disabled"></div>
-            <div class="owl-dots"></div>
-          </div>
-        </div>
-      </article>
-    </section>
-  </div>
-`;
+  // Create the carousel element [2]
+  var carouselElement = document.createElement('div');
+  carouselElement.classList.add('cmp-carousel');
+  carouselElement.setAttribute('data-cmp-delay', '5000');
 
-// Get the carousel container and its inner elements
-const carouselContainer = newElement.querySelector('.owl-carousel');
-const owlStage = carouselContainer.querySelector('.owl-stage');
-const owlItem = owlStage.querySelector('.owl-item.cloned');
+  // Create the section element [2]
+  var sectionElement = document.createElement('section');
+  sectionElement.setAttribute('id', 'hero_slider');
 
-export default function decorate(block) {
-[...block.children].forEach((item) => {
-const picture = item.querySelector('picture');
-  const imgSrc = picture.querySelector('img').getAttribute('src');
-  const imgAlt = picture.querySelector('img').getAttribute('alt');
-  const buttonHref = item.querySelector('.button-container > a').getAttribute('href');
-  const buttonTitle = item.querySelector('.button-container > a').getAttribute('title');
-  const description = item.querySelector('div:nth-child(3)').textContent;
+  // Create the main content container element [2]
+  var mainContentContainer = document.createElement('div');
+  mainContentContainer.setAttribute('id', 'main-cnt');
 
-  const carouselItem = document.createElement('div');
-  carouselItem.className = 'item slide-2';
-  carouselItem.innerHTML = `
-    <div class="teaser">
-      <img class="portal-mob visible-xs" src="/content/dam/infosys-web/en/global-resource/background-image/portals.png" alt="Portals">
-      <div role="img" aria-label="${description}" class="repeated-css industries-gradient">
-        <img src="${imgSrc}" class="slide-image hidden-xs" alt="${imgAlt}">
-        <img src="${imgSrc}" class="slide-image visible-xs" alt="${imgAlt}">
-      </div>
-      <div class="slider-overlay-coral"></div>
-      <div class="caption">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-7 col-md-6 col-sm-6 col-xs-12 wow fadeInLeft animated animated" style="visibility: visible;">
-              <p class="text-uppercase topicIndicator">${buttonTitle}</p>
-              <p class="h2-heading white-color">${description}</p>
-              <a href="${buttonHref}" target="_blank" class="btn btn-shutter-more text-uppercase" aria-describedby="${buttonTitle}">Learn More</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
-  owlStage.insertBefore(carouselItem, owlItem);
-  });
-}
+  // Append the main content container to the section element
+  sectionElement.appendChild(mainContentContainer);
 
-// Replace the target element with the new element
-targetElement.parentNode.replaceChild(newElement, targetElement);
+  // Create the article element [2]
+  var articleElement = document.createElement('article');
+  articleElement.classList.add('container-fluid', 'slider');
+
+  // Create the row element [2]
+  var rowElement = document.createElement('div');
+  rowElement.classList.add('row');
+
+  // Append the row element to the article element
+  articleElement.appendChild(rowElement);
+
+  // Get the individual carousel items from [1]
+  var carouselItems = targetElement.querySelectorAll('div > div');
+
+  // Loop through each carousel item from [1]
+  for (var i = 0; i < carouselItems.length; i++) {
+    var item = carouselItems[i];
+
+    // Create the owl-item element [2]
+    var owlItemElement = document.createElement('div');
+    owlItemElement.classList.add('owl-item');
+    owlItemElement.style.width = '1263.33px';
+
+    // Create the item element [2]
+    var itemElement = document.createElement('div');
+    itemElement.classList.add('item', 'slide-' + i);
+
+    // Clone the contents of each carousel item and append them to the item element
+    var clonedContents = item.cloneNode(true).childNodes;
+    for (var j = 0; j < clonedContents.length; j++) {
+      itemElement.appendChild(clonedContents[j].cloneNode(true));
+    }
+
+    // Append the item element to the owl-item element
+    owlItemElement.appendChild(itemElement);
+
+    // Append the owl-item element to the row element
+    rowElement.appendChild(owlItemElement);
+  }
+
+  // Append the row element to the article element
+  articleElement.appendChild(rowElement);
+
+  // Append the article element to the main container
+  mainContainer.appendChild(articleElement);
+
+  // Append the section element to the carousel element
+  carouselElement.appendChild(sectionElement);
+
+  // Append the carousel element to the main container
+  mainContainer.appendChild(carouselElement);
+
+  // Replace the original target element with the converted carousel [2]
+  targetElement.parentNode.replaceChild(mainContainer, targetElement);
