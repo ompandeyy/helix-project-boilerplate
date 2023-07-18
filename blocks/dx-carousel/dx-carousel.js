4d15b6cb0041aca28b5456d2399e6d2bab47bfb5
ompandeyy <ompandeyinfy@gmail.com>
2023-07-18 11:17:54 +0200

    Update dx-carousel.js

--- blocks/dx-carousel/dx-carousel.js
+++ blocks/dx-carousel/dx-carousel.js
@@ -12,15 +12,25 @@ newElement.innerHTML = `
       <div id="main-cnt"></div>
       <article class="container-fluid slider">
         <div class="row">
-          <div id="hero_slider_carousel" class="owl-carousel owl-theme owl-loaded owl-drag"></div>
+          <div id="hero_slider_carousel" class="owl-carousel owl-theme owl-loaded owl-drag">
+            <div class="owl-stage-outer">
+              <div class="owl-stage">
+                <div class="owl-item cloned" style="width: 1263.33px;"></div>
+              </div>
+            </div>
+            <div class="owl-nav disabled"></div>
+            <div class="owl-dots"></div>
+          </div>
         </div>
       </article>
     </section>
   </div>
 `;
 
-// Get the carousel container
-const carouselContainer = newElement.querySelector('#hero_slider_carousel');
+// Get the carousel container and its inner elements
+const carouselContainer = newElement.querySelector('.owl-carousel');
+const owlStage = carouselContainer.querySelector('.owl-stage');
+const owlItem = owlStage.querySelector('.owl-item.cloned');
 
 export default function decorate(block) {
 [...block.children].forEach((item) => {
@@ -32,32 +42,29 @@ const picture = item.querySelector('picture');
   const description = item.querySelector('div:nth-child(3)').textContent;
 
   const carouselItem = document.createElement('div');
-  carouselItem.className = 'owl-item cloned';
-  carouselItem.style.width = '1263.33px';
+  carouselItem.className = 'item slide-2';
   carouselItem.innerHTML = `
-    <div class="item slide-2">
-      <div class="teaser">
-        <img class="portal-mob visible-xs" src="/content/dam/infosys-web/en/global-resource/background-image/portals.png" alt="Portals">
-        <div role="img" aria-label="${description}" class="repeated-css industries-gradient">
-          <img src="${imgSrc}" class="slide-image hidden-xs" alt="${imgAlt}">
-          <img src="${imgSrc}" class="slide-image visible-xs" alt="${imgAlt}">
-        </div>
-        <div class="slider-overlay-coral"></div>
-        <div class="caption">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-7 col-md-6 col-sm-6 col-xs-12 wow fadeInLeft animated animated" style="visibility: visible;">
-                <p class="text-uppercase topicIndicator">${buttonTitle}</p>
-                <p class="h2-heading white-color">${description}</p>
-                <a href="${buttonHref}" target="_blank" class="btn btn-shutter-more text-uppercase" aria-describedby="${buttonTitle}">Learn More</a>
-              </div>
+    <div class="teaser">
+      <img class="portal-mob visible-xs" src="/content/dam/infosys-web/en/global-resource/background-image/portals.png" alt="Portals">
+      <div role="img" aria-label="${description}" class="repeated-css industries-gradient">
+        <img src="${imgSrc}" class="slide-image hidden-xs" alt="${imgAlt}">
+        <img src="${imgSrc}" class="slide-image visible-xs" alt="${imgAlt}">
+      </div>
+      <div class="slider-overlay-coral"></div>
+      <div class="caption">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-7 col-md-6 col-sm-6 col-xs-12 wow fadeInLeft animated animated" style="visibility: visible;">
+              <p class="text-uppercase topicIndicator">${buttonTitle}</p>
+              <p class="h2-heading white-color">${description}</p>
+              <a href="${buttonHref}" target="_blank" class="btn btn-shutter-more text-uppercase" aria-describedby="${buttonTitle}">Learn More</a>
             </div>
           </div>
         </div>
       </div>
     </div>
   `;
-  carouselContainer.appendChild(carouselItem);
+  owlStage.insertBefore(carouselItem, owlItem);
   });
 }
 
